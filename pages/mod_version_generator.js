3133036d9c739a2f447499fe794308fb03ddaa90
electric131 <39623923+Electric131@users.noreply.github.com>
2025-04-06 17:44:12 -0500

    Added README extraction for mods

--- pages/mod_version_generator.js
+++ pages/mod_version_generator.js
@@ -1,206 +1,184 @@
-const colors = require('colors');
-const http = require('http');
-const JSZip = require('jszip');
-const Utils = require('./../utils');
+const colors = require("colors");
+const http = require("http");
+const JSZip = require("jszip");
+const Utils = require("./../utils");
 
-const log = new Utils.log.log(
-    colors.green('Sandustry.web.pages.generateMod'),
-    './sandustry.web.main.txt',
-    true
-);
+const log = new Utils.log.log(colors.green("Sandustry.web.pages.generateMod"), "./sandustry.web.main.txt", true);
 
 module.exports = {
-    paths: ['/generateMod'],
-    run: async function (req, res) {
-        try {
-            // Function to generate random strings
-            const randomString = (length) =>
-                Math.random().toString(36).substring(2, 2 + length);
+	paths: ["/generateMod"],
+	run: async function (req, res) {
+		try {
+			// Function to generate random strings
+			const randomString = (length) =>
+				Math.random()
+					.toString(36)
+					.substring(2, 2 + length);
 
-            // Function to increment semantic version
-            const incrementSemVer = ({ major, minor, patch }) => {
-                const type = Math.floor(Math.random() * 3); // 0 = major, 1 = minor, 2 = patch
-                if (type === 0) {
-                    return { major: major + 1, minor: 0, patch: 0 };
-                } else if (type === 1) {
-                    return { major, minor: minor + 1, patch: 0 };
-                } else {
-                    return { major, minor, patch: patch + 1 };
-                }
-            };
+			// Function to increment semantic version
+			const incrementSemVer = ({ major, minor, patch }) => {
+				const type = Math.floor(Math.random() * 3); // 0 = major, 1 = minor, 2 = patch
+				if (type === 0) {
+					return { major: major + 1, minor: 0, patch: 0 };
+				} else if (type === 1) {
+					return { major, minor: minor + 1, patch: 0 };
+				} else {
+					return { major, minor, patch: patch + 1 };
+				}
+			};
 
-            // Predefined static list of 25 realistic tags
-            const predefinedTags = [
-                'multiplayer',
-                'sandbox',
-                'mod',
-                'game',
-                'strategy',
-                'action',
-                'fun',
-                'creative',
-                'builder',
-                'survival',
-                'teamwork',
-                'indie',
-                'hardcore',
-                'simulation',
-                'adventure',
-                'puzzle',
-                'co-op',
-                'realistic',
-                'casual',
-                'competitive',
-                'retro',
-                'fast-paced',
-                'rpg',
-                'platformer',
-                'tactical',
-            ];
+			// Predefined static list of 25 realistic tags
+			const predefinedTags = [
+				"multiplayer",
+				"sandbox",
+				"mod",
+				"game",
+				"strategy",
+				"action",
+				"fun",
+				"creative",
+				"builder",
+				"survival",
+				"teamwork",
+				"indie",
+				"hardcore",
+				"simulation",
+				"adventure",
+				"puzzle",
+				"co-op",
+				"realistic",
+				"casual",
+				"competitive",
+				"retro",
+				"fast-paced",
+				"rpg",
+				"platformer",
+				"tactical",
+			];
 
-            // Function to generate tags for a version
-            const generateTagsForVersion = (previousTags = null) => {
-                // Number of tags to use for this version
-                const numTags = Math.floor(Math.random() * 6) + 10; // 10 to 15 tags
-                const tags = previousTags ? [...previousTags] : [];
+			// Function to generate tags for a version
+			const generateTagsForVersion = (previousTags = null) => {
+				// Number of tags to use for this version
+				const numTags = Math.floor(Math.random() * 6) + 10; // 10 to 15 tags
+				const tags = previousTags ? [...previousTags] : [];
 
-                // If this isn't the first version, randomly replace some tags
-                if (previousTags) {
-                    const numReplacements = Math.floor(Math.random() * 4) + 1; // Replace 1-3 tags
-                    for (let i = 0; i < numReplacements; i++) {
-                        // Remove a random tag and replace it
-                        const indexToReplace = Math.floor(
-                            Math.random() * tags.length
-                        );
-                        const newTag =
-                            predefinedTags[
-                                Math.floor(Math.random() * predefinedTags.length)
-                                ];
-                        tags[indexToReplace] = newTag;
-                    }
-                } else {
-                    // For the first version, generate unique random tags
-                    while (tags.length < numTags) {
-                        const newTag =
-                            predefinedTags[
-                                Math.floor(Math.random() * predefinedTags.length)
-                                ];
-                        if (!tags.includes(newTag)) {
-                            tags.push(newTag);
-                        }
-                    }
-                }
+				// If this isn't the first version, randomly replace some tags
+				if (previousTags) {
+					const numReplacements = Math.floor(Math.random() * 4) + 1; // Replace 1-3 tags
+					for (let i = 0; i < numReplacements; i++) {
+						// Remove a random tag and replace it
+						const indexToReplace = Math.floor(Math.random() * tags.length);
+						const newTag = predefinedTags[Math.floor(Math.random() * predefinedTags.length)];
+						tags[indexToReplace] = newTag;
+					}
+				} else {
+					// For the first version, generate unique random tags
+					while (tags.length < numTags) {
+						const newTag = predefinedTags[Math.floor(Math.random() * predefinedTags.length)];
+						if (!tags.includes(newTag)) {
+							tags.push(newTag);
+						}
+					}
+				}
 
-                return tags;
-            };
+				return tags;
+			};
 
-            // Function to generate a random modinfo.json file
-            const generateModInfo = (modName, version, tags) => {
-                return {
-                    name: modName,
-                    version: version,
-                    author: `author-${randomString(8)}`,
-                    shortDescription: `A test mod for the Electron Modloader`,
-                    description: `# Test Mod\nA test mod for the Electron Modloader`,
-                    modloaderVersion: '>=1.0.0',
-                    dependencies: {
-                        [`module-${randomString(6)}`]: `=1.0.0`,
-                        [`module-${randomString(6)}`]: `=2.0.0`,
-                    },
-                    tags: tags,
-                    electronEntrypoint: 'entry.electron.js',
-                    browserEntrypoint: 'entry.browser.js',
-                    workerEntrypoint: 'entry.worker.js',
-                    defaultConfig: {
-                        someSetting: true,
-                        someValue: Math.floor(Math.random() * 1000),
-                    },
-                };
-            };
+			// Function to generate a random modinfo.json file
+			const generateModInfo = (modName, version, tags) => {
+				return {
+					name: modName,
+					version: version,
+					author: `author-${randomString(8)}`,
+					shortDescription: `A test mod for the Electron Modloader`,
+					modloaderVersion: ">=1.0.0",
+					dependencies: {
+						[`module-${randomString(6)}`]: `=1.0.0`,
+						[`module-${randomString(6)}`]: `=2.0.0`,
+					},
+					tags: tags,
+					electronEntrypoint: "entry.electron.js",
+					browserEntrypoint: "entry.browser.js",
+					workerEntrypoint: "entry.worker.js",
+					defaultConfig: {
+						someSetting: true,
+						someValue: Math.floor(Math.random() * 1000),
+					},
+				};
+			};
 
-            // Generate a random number of versions between 1 and 5
-            const numVersions = Math.floor(Math.random() * 5) + 1;
+			// Generate a random number of versions between 1 and 5
+			const numVersions = Math.floor(Math.random() * 5) + 1;
 
-            // Create a main zip archive to hold all version zips
-            const mainZip = new JSZip();
+			// Create a main zip archive to hold all version zips
+			const mainZip = new JSZip();
 
-            // Generate a random mod name
-            const modName = `testmod-${randomString(5)}`;
+			// Generate a random mod name
+			const modName = `testmod-${randomString(5)}`;
 
-            // Start with the first version
-            let currentVersion = { major: 1, minor: 0, patch: 0 };
+			// Start with the first version
+			let currentVersion = { major: 1, minor: 0, patch: 0 };
 
-            // Tags for the first version
-            let currentTags = generateTagsForVersion();
+			// Tags for the first version
+			let currentTags = generateTagsForVersion();
 
-            // Generate `.zip` files for each version
-            for (let i = 0; i < numVersions; i++) {
-                const versionString = `${currentVersion.major}.${currentVersion.minor}.${currentVersion.patch}`;
+			// Generate `.zip` files for each version
+			for (let i = 0; i < numVersions; i++) {
+				const versionString = `${currentVersion.major}.${currentVersion.minor}.${currentVersion.patch}`;
 
-                // Create a zip archive for this version
-                const versionZip = new JSZip();
+				// Create a zip archive for this version
+				const versionZip = new JSZip();
 
-                // Add modinfo.json to this version archive
-                const modInfo = generateModInfo(modName, versionString, currentTags);
-                versionZip.file(
-                    'modinfo.json',
-                    JSON.stringify(modInfo, null, 2) // Prettify JSON
-                );
+				// Add modinfo.json to this version archive
+				const modInfo = generateModInfo(modName, versionString, currentTags);
+				versionZip.file(
+					"modinfo.json",
+					JSON.stringify(modInfo, null, 2) // Prettify JSON
+				);
 
-                // Add entry point files to the version zip
-                versionZip.file(
-                    'entry.electron.js',
-                    `// Electron entry point for version ${versionString}`
-                );
-                versionZip.file(
-                    'entry.browser.js',
-                    `// Browser entry point for version ${versionString}`
-                );
-                versionZip.file(
-                    'entry.worker.js',
-                    `// Worker entry point for version ${versionString}`
-                );
+				// Add entry point files to the version zip
+				versionZip.file("entry.electron.js", `// Electron entry point for version ${versionString}`);
+				versionZip.file("entry.browser.js", `// Browser entry point for version ${versionString}`);
+				versionZip.file("entry.worker.js", `// Worker entry point for version ${versionString}`);
 
-                // Generate the content of this version's zip
-                const versionZipContent = await versionZip.generateAsync({
-                    type: 'nodebuffer',
-                });
+				// Add README
+				versionZip.file("README.md", `# Test Mod\nA test mod for the Electron Modloader`);
 
-                // Add this version zip to the main zip archive
-                mainZip.file(
-                    `${modName}-v${versionString}.zip`,
-                    versionZipContent
-                );
+				// Generate the content of this version's zip
+				const versionZipContent = await versionZip.generateAsync({
+					type: "nodebuffer",
+				});
 
-                // Increment to the next version
-                currentVersion = incrementSemVer(currentVersion);
+				// Add this version zip to the main zip archive
+				mainZip.file(`${modName}-v${versionString}.zip`, versionZipContent);
 
-                // Generate tags for the next version (with some changes)
-                currentTags = generateTagsForVersion(currentTags);
-            }
+				// Increment to the next version
+				currentVersion = incrementSemVer(currentVersion);
 
-            // Generate the main zip file
-            const mainZipContent = await mainZip.generateAsync({
-                type: 'nodebuffer',
-            });
+				// Generate tags for the next version (with some changes)
+				currentTags = generateTagsForVersion(currentTags);
+			}
 
-            // Set correct headers and send the main zip file
-            res.writeHead(200, {
-                'Content-Type': 'application/zip',
-                'Content-Disposition': `attachment; filename=${modName}-versions.zip`,
-            });
+			// Generate the main zip file
+			const mainZipContent = await mainZip.generateAsync({
+				type: "nodebuffer",
+			});
 
-            res.end(mainZipContent);
+			// Set correct headers and send the main zip file
+			res.writeHead(200, {
+				"Content-Type": "application/zip",
+				"Content-Disposition": `attachment; filename=${modName}-versions.zip`,
+			});
 
-            // Log success
-            log.success(
-                `Generated mod with ${numVersions} version zips: ${modName}`
-            );
-        } catch (error) {
-            // Handle errors here
-            log.error('Error generating mod versions:', error);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Failed to generate mod versions' }));
-        }
-    },
+			res.end(mainZipContent);
+
+			// Log success
+			log.success(`Generated mod with ${numVersions} version zips: ${modName}`);
+		} catch (error) {
+			// Handle errors here
+			log.error("Error generating mod versions:", error);
+			res.writeHead(500, { "Content-Type": "application/json" });
+			res.end(JSON.stringify({ error: "Failed to generate mod versions" }));
+		}
+	},
 };
--- pages/uploadmod.js
+++ pages/uploadmod.js
@@ -1,159 +1,165 @@
-var colors = require('colors');
-var http = require("http")
-var os = require("os")
-var Websocket = require("ws")
-var crypto = require("crypto")
-var util = require('util')
-var fs = require('fs')
-var ejs = require('ejs')
-var { exec } = require('child_process');
-var Utils = require('./../utils')
+var colors = require("colors");
+var http = require("http");
+var os = require("os");
+var Websocket = require("ws");
+var crypto = require("crypto");
+var util = require("util");
+var fs = require("fs");
+var ejs = require("ejs");
+var { exec } = require("child_process");
+var Utils = require("./../utils");
 var { MongoClient } = require("mongodb");
 var { compress } = require("@mongodb-js/zstd");
-var https = require('https');
+var https = require("https");
 var JSZip = require("jszip");
 var log = new Utils.log.log(colors.green("Sandustry.web.pages.upload"), "./sandustry.web.main.txt", true);
 var mongoUri = globalThis.Config.mongodb.uri;
-var sanitizeHtml = require('sanitize-html');
+var sanitizeHtml = require("sanitize-html");
 module.exports = {
-    paths: ['/api/uploadmod'],
-    run: function (req, res) {
-        if (req.method !== "POST") {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.end("This is an API endpoint.");
-            return;
-        }
+	paths: ["/api/uploadmod"],
+	run: function (req, res) {
+		if (req.method !== "POST") {
+			res.writeHead(404, { "Content-Type": "text/html" });
+			res.end("This is an API endpoint.");
+			return;
+		}
 
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
+		let body = "";
+		req.on("data", (chunk) => {
+			body += chunk.toString();
+		});
 
-        req.on('end', async () => {
-            var client = new MongoClient(mongoUri);
+		req.on("end", async () => {
+			var client = new MongoClient(mongoUri);
 
-            try {
-                var payload = await JSON.parse(body);
-                var { filename, filedata, discordInfo } = payload;
-                var modID = crypto.randomUUID();
-                var newmod = false;
-                if(payload.modID){
-                    newmod = true;
-                    modID = payload.modID
-                }
-                if (!filename || !filedata) {
-                    throw new Error('Invalid payload. "filename" and "filedata" are required.');
-                }
-                if (!discordInfo || !discordInfo.id || !discordInfo.tokenResponse || !discordInfo.tokenResponse.access_token) {
-                    throw new Error('Invalid "discordInfo". Discord user information with an access token is required.');
-                }
-                var isValidUser = await verifyDiscordUser(discordInfo.id, discordInfo.tokenResponse.access_token);
-                if (!isValidUser) {
-                    throw new Error('Discord user validation failed. The provided user cannot be verified.');
-                }
+			try {
+				var payload = await JSON.parse(body);
+				var { filename, filedata, discordInfo } = payload;
+				var modID = crypto.randomUUID();
+				var newmod = false;
+				if (payload.modID) {
+					newmod = true;
+					modID = payload.modID;
+				}
+				if (!filename || !filedata) {
+					throw new Error('Invalid payload. "filename" and "filedata" are required.');
+				}
+				if (!discordInfo || !discordInfo.id || !discordInfo.tokenResponse || !discordInfo.tokenResponse.access_token) {
+					throw new Error('Invalid "discordInfo". Discord user information with an access token is required.');
+				}
+				var isValidUser = await verifyDiscordUser(discordInfo.id, discordInfo.tokenResponse.access_token);
+				if (!isValidUser) {
+					throw new Error("Discord user validation failed. The provided user cannot be verified.");
+				}
 
-                var zipBuffer = Buffer.from(filedata, "base64");
+				var zipBuffer = Buffer.from(filedata, "base64");
 
-                var compressedZipBuffer = await compress(zipBuffer,10);
-                //upload to mongodb
-                await client.connect();
-                var db = client.db("SandustryMods");
-                var modsCollection = db.collection("Mods");
-                var versionsCollection = db.collection("ModVersions");
+				var compressedZipBuffer = await compress(zipBuffer, 10);
+				//upload to mongodb
+				await client.connect();
+				var db = client.db("SandustryMods");
+				var modsCollection = db.collection("Mods");
+				var versionsCollection = db.collection("ModVersions");
 
+				var content = await JSZip.loadAsync(zipBuffer);
+				var fileNames = Object.keys(content.files);
+				var modInfoPath = fileNames.find((path) => path.endsWith("modinfo.json"));
+				var modInfoFile = await content.file(modInfoPath);
+				var modInfoContent = await modInfoFile.async("text");
+				var modinfo = await JSON.parse(modInfoContent);
+				var readmePath = fileNames.find((path) => path.endsWith("README.md"));
+				var readmeFile = await content.file(readmePath);
+				var rawDescription = await readmeFile.async("text");
+				var compressedDescription = (await compress(Buffer.from(rawDescription), 10)).toString("base64");
+				Object.keys(modinfo).forEach((key) => {
+					if (typeof modinfo[key] === "string") {
+						modinfo[key] = sanitizeHtml(modinfo[key]);
+					}
+				});
+				var modEntry = {};
+				modEntry = await modsCollection.findOne({ modID: modID, "Author.discordID": discordInfo.id });
+				if (!modEntry) {
+					modEntry = await modsCollection.findOne({ "modinfo.name": modinfo.name, "Author.discordID": discordInfo.id });
+				}
+				modID = modEntry ? modEntry.modID : modID;
+				if (!modEntry) {
+					modEntry = {
+						modID: modID,
+						modinfo: modinfo,
+						Author: {
+							discordID: discordInfo.id,
+							discordUsername: discordInfo.username,
+						},
+					};
+					await modsCollection.insertOne(modEntry);
+				}
+				var modVersionEntry = {
+					modID: modEntry.modID,
+					modfile: compressedZipBuffer.toString("base64"),
+					modinfo: modinfo,
+					description: compressedDescription,
+					uploadTime: new Date(),
+					downloadCount: 0,
+				};
+				await versionsCollection.insertOne(modVersionEntry);
 
-                var content = await JSZip.loadAsync(zipBuffer);
-                var fileNames = Object.keys(content.files);
-                var modInfoPath = fileNames.find((path) => path.endsWith('modinfo.json'));
-                var modInfoFile = await content.file(modInfoPath);
-                var modInfoContent = await modInfoFile.async('text');
-                var modinfo = await JSON.parse(modInfoContent);
-                Object.keys(modinfo).forEach(key => {
-                    if(typeof modinfo[key] === "string"){
-                        modinfo[key] = sanitizeHtml(modinfo[key]);
-                    }
-                });
-                var modEntry = {}
-                modEntry = await modsCollection.findOne({ "modID": modID, "Author.discordID": discordInfo.id, });
-                if (!modEntry) {
-                    modEntry = await modsCollection.findOne({ "modinfo.name": modinfo.name, "Author.discordID": discordInfo.id });
-                }
-                modID = modEntry ? modEntry.modID : modID;
-                if (!modEntry) {
-                    modEntry = {
-                        modID: modID,
-                        modinfo: modinfo,
-                        Author: {
-                            discordID: discordInfo.id,
-                            discordUsername: discordInfo.username
-                        }
-                    };
-                    await modsCollection.insertOne(modEntry);
-                }
-                var modVersionEntry = {
-                    modID: modEntry.modID,
-                    modfile: compressedZipBuffer.toString("base64"),
-                    modinfo: modinfo,
-                    uploadTime: new Date(),
-                    downloadCount: 0
-                };
-                await versionsCollection.insertOne(modVersionEntry);
+				await res.writeHead(200, { "Content-Type": "application/json" });
+				await res.end(
+					JSON.stringify({
+						message: `File ${filename} uploaded successfully.`,
+					})
+				);
+			} catch (error) {
+				console.error("Error processing upload:", error);
 
-                await res.writeHead(200, { "Content-Type": "application/json" });
-                await res.end(JSON.stringify({
-                    message: `File ${filename} uploaded successfully.`
-                }));
-            } catch (error) {
-                console.error('Error processing upload:', error);
-
-                await res.writeHead(400, { "Content-Type": "application/json" });
-                await res.end(JSON.stringify({ error: error.message }));
-            }
-        });
-    }
-}
+				await res.writeHead(400, { "Content-Type": "application/json" });
+				await res.end(JSON.stringify({ error: error.message }));
+			}
+		});
+	},
+};
 
 function verifyDiscordUser(userId, accessToken) {
-    return new Promise((resolve, reject) => {
-        var options = {
-            hostname: 'discord.com',
-            path: '/api/users/@me',
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        };
+	return new Promise((resolve, reject) => {
+		var options = {
+			hostname: "discord.com",
+			path: "/api/users/@me",
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			},
+		};
 
-        var req = https.request(options, (res) => {
-            let data = '';
+		var req = https.request(options, (res) => {
+			let data = "";
 
-            res.on('data', (chunk) => {
-                data += chunk.toString();
-            });
+			res.on("data", (chunk) => {
+				data += chunk.toString();
+			});
 
-            res.on('end', () => {
-                try {
-                    var userResponse = JSON.parse(data);
+			res.on("end", () => {
+				try {
+					var userResponse = JSON.parse(data);
 
-                    // Check if the user ID matches the ID from the token response
-                    if (res.statusCode === 200 && userResponse.id === userId) {
-                        resolve(true);
-                    } else {
-                        console.warn('User ID mismatch or token is invalid:', userResponse);
-                        resolve(false);
-                    }
-                } catch (err) {
-                    console.error('Error parsing user verification response:', err);
-                    resolve(false);
-                }
-            });
-        });
+					// Check if the user ID matches the ID from the token response
+					if (res.statusCode === 200 && userResponse.id === userId) {
+						resolve(true);
+					} else {
+						console.warn("User ID mismatch or token is invalid:", userResponse);
+						resolve(false);
+					}
+				} catch (err) {
+					console.error("Error parsing user verification response:", err);
+					resolve(false);
+				}
+			});
+		});
 
-        req.on('error', (err) => {
-            console.error('Error verifying Discord user:', err);
-            resolve(false);
-        });
+		req.on("error", (err) => {
+			console.error("Error verifying Discord user:", err);
+			resolve(false);
+		});
 
-        req.end();
-    });
+		req.end();
+	});
 }
