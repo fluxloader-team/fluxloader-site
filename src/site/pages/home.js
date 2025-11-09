ef5db1d51a3af8adc25eb8f58180f7d7c2dc42d2
tomster12 <thomas.burniston@gmail.com>
2025-11-09 01:38:12 +0000

    formatting

--- src/site/pages/home.js
+++ src/site/pages/home.js
@@ -9,10 +9,8 @@ module.exports = {
 	run: async function (req, res) {
 		const session = await getSessionFromRequest(req);
 		const user = session != null ? await DB.users.one(session.discordID) : null;
-
 		const tpl = globalThis.templates["home.ejs"];
 		const html = ejs.render(tpl.content, { include: includeFromMemory, user }, { filename: tpl.path });
-
 		res.writeHead(200, { "Content-Type": "text/html" });
 		res.end(html);
 	},
