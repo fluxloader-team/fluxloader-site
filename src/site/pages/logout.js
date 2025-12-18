3eb05a5a319b5de30380844689b8d6be676ef060
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2025-12-18 14:59:47 +0000

    Deleting the session when logging out

--- src/site/pages/logout.js
+++ src/site/pages/logout.js
@@ -1,6 +1,15 @@
+const { getSessionFromRequest, removeSession } = require("../../common/session");
+
 module.exports = {
 	paths: ["/logout"],
 	run: async (req, res) => {
+		const session = await getSessionFromRequest(req)
+
+		// Deleting the session from the DB
+		if (session != null) {
+			await removeSession(session.token)
+		}
+
 		res.writeHead(302, {
 			Location: "/",
 			"Set-Cookie": "session=; Path=/; Max-Age=0; HttpOnly",
