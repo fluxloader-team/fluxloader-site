ed4a91395c6d2146ae103a4618eb41f472b5e678
tomster12 <thomas.burniston@gmail.com>
2025-12-18 19:41:41 +0000

    Remove other run func

--- src/site/pages/upload.js
+++ src/site/pages/upload.js
@@ -6,14 +6,6 @@ const { getSessionFromRequest } = require("../../common/session");
 module.exports = {
 	paths: ["/upload"],
 
-	run: function (req, res) {
-		const tpl = globalThis.templates["upload.ejs"];
-		const html = ejs.render(tpl.content, { include: ejsExtensions.includeFromMemory }, { filename: tpl.path });
-
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.end(html);
-	},
-
 	run: async function (req, res) {
 		const session = await getSessionFromRequest(req);
 		const user = session != null ? await DB.users.one(session.discordID) : null;
